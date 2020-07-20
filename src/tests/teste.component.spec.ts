c61f9e66f2f5d5e47679a2b7134b041865240e1b
Fabio <fabiogarcia92@gmail.com>
2020-07-20 03:24:06 -0300

    Correção de testes

--- src/tests/teste.component.spec.ts
+++ src/tests/teste.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TesteComponent } from '../../teste/teste.component';
-import {GithubService} from '../services/github.service';
+import { TesteComponent} from '../app/teste/teste.component';
+import {GithubService} from '../app/shared/services/github.service';
 import {HttpClientModule} from '@angular/common/http';
 
 describe('TesteComponent', () => {
